645165a239d839b981f19dfe3a5063d1768033f1
Kirill <anashkin.kirill123@mail.ru>
2022-10-20 15:16:08 +0300

    laba

---
+++ index.js
@@ -0,0 +1,64 @@
+// Задание 1
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// let result = sum(5, 7);
+// alert(result);
+
+// Задание 2
+// function type() {
+//     let a = document.getElementById('s1').value;
+//     alert(a);
+// }
+
+// Задание 2
+// console.log(typeof "hello");
+// console.log(typeof 228);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof Symbol("id"));
+// console.log(typeof alert);
+// console.log(typeof null);
+// console.log(typeof BigInt(10));
+// console.log(undefined == null);
+// console.log(undefined === null);
+
+// Задание 3.
+// Array.apply(null, Array(10)).map(function() { return Math.floor(Math.random() * 100 % 100); })
+// if (Array.apply >= 0) {
+//     numbers.splice(i, 1);
+// };
+
+// Задание 4.
+// Array.apply(null, Array(100)).map(function() { return Math.floor(Math.random() * 100 % 94 + 6); })
+// if (Array.apply >= 0) {
+//     numbers.splice(i, 1);
+// };
+
+
+// Задание 5.
+// function arr() {
+//     let arrN = ["Кирилл", "Сергей", "Екатерина", "Виктория", "Дмитрий", "Дарина", "Виктор", "Максим", "Ирина", "Давид"];
+
+//     return arrN;
+// }
+// console.log(arr());
+
+// Задание 6.
+// let date1 = new Date('2021/11/01 00:00:00');
+// let date2 = new Date('2021/11/11 10:10:10');
+// let start = Math.floor(date1.getTime() / (3600 * 24 * 1000));
+// let end = Math.floor(date2.getTime() / (3600 * 24 * 1000));
+// let daysDiff = end - start;
+// console.log(daysDiff);
+
+// Задание 7.
+// let array = ["Укроп", "Небоскрёб", "Гусар", "Кенгуру", "Царь", "Ель"];
+
+// function func(a, b) {
+//     return 0.5 - Math.random();
+// }
+// array.forEach((el, index) => {
+//     console.log(`${index} - ${el}`);
+// });
